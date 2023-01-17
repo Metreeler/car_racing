258b3b041247f924550fb26e31e5218fc0acd64b
Malo HANGRAN <malo.lyckens@laposte.net>
2023-01-17 14:18:45 +0100

    Adding the menu

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -2,8 +2,17 @@ import pyglet
 from my_window import MyWindow
 
 frameRate = 60.0
+playing_editing = True
 
 if __name__ == "__main__":
-    window = MyWindow(1080, 720, "Car Game", resizable=True)
-    pyglet.clock.schedule_interval(window.update, 1 / frameRate)
-    pyglet.app.run()
+    while playing_editing:
+        i = input("Do you want to play (p) or do you want to create map (m) : ")
+        if i == "p":
+            playing_editing = False
+            window = MyWindow(1080, 720, "Car Game", resizable=True)
+            pyglet.clock.schedule_interval(window.update, 1 / frameRate)
+            pyglet.app.run()
+        elif i == "m":
+            playing_editing = False
+        else:
+            print(i + " is not a valid answer")
